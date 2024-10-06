98eab77fde0e091483c34f827bc4ee62b4a14daa
Mauro <80912185+mauro-romanin-malignani-ud@users.noreply.github.com>
2024-10-06 18:01:22 +0000

    modified:   .vscode/tasks.json 	modified:   UD4.LAB2R/UD4.LAB2R.c

--- UD4.LAB2R/UD4.LAB2R.c
+++ UD4.LAB2R/UD4.LAB2R.c
@@ -2,6 +2,25 @@
 #include <stdio.h>
 #include <math.h>
 
+//gcc test.c -o test -lm
+/*
+FIX:    undefined reference to `sqrt'
+        collect2: error: ld returned 1 exit status
+
+Se si utilizza la libreria <math.h> il link non include la libreria e bisogna aggiungere l'argomento -lm al compilatore gcc    
+
+Nella cartella .vscode, editare il file task.json e nel task relativo al 
+    "command": "/usr/bin/gcc"
+aggiungere fra gli "args" la voce "-lm"
+
+/usr/bin/gcc -fdiagnostics-color=always -g /workspaces/4SER.24-25/UD4.LAB2R/UD4.LAB2R.c -o /workspaces/4SER.24-25/UD4.LAB2R/UD4.LAB2R -lm
+
+./UD4.LAB2R/UD4.LAB2R
+
+*/
+
+
+
 void main(){
     printf("UD4.LAB2R\n");
 
